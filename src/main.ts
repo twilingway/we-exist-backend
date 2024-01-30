cf5766efaa89a38c0f257d37eea5417f7d8bc1a6
Aleksei Glazunov <993468@gmail.com>
2024-01-30 13:04:40 +0400

    edic cors settings

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,11 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule, { logger: ['error', 'warn'] });
     app.useGlobalPipes(new ValidationPipe());
     app.setGlobalPrefix('api');
-    app.enableCors();
+    app.enableCors({
+        origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+        // methods: ['GET', 'POST'],
+        credentials: true,
+    });
     app.use(cookieParser());
     app.useGlobalInterceptors();
     app.enableShutdownHooks();
